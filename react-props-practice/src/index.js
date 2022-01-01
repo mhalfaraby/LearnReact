de6c5f8ab6659aa74705d1459670b6772d0684f2
mhalfaraby <mhalfaraby@gmail.com>
2022-01-02 01:45:28 +0700

    finish the challenge

--- react-props-practice/src/index.js
+++ react-props-practice/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Card from "./components/Card";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
